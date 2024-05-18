1e913d5bc1a6cbec444a0dafede410566b366f2f
Juan Morena <thewolfmodzyt@gmail.com>
2024-05-18 13:26:59 -0300

    Archivos template creados

---
+++ entities/policlinica.py
@@ -0,0 +1,15 @@
+class Policlinica:
+  def __init__(self):
+    pass
+
+  def dar_alta_medico(self):
+    pass
+
+  def dar_alta_consulta(self):
+    pass
+
+  def emitir_ticket(self):
+    pass
+
+  def realizar_consulta(self):
+    pass
