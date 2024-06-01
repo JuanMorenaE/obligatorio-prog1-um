a8d39e0def8f195ec126f2365fe8ede893408f6e
Juan Morena <thewolfmodzyt@gmail.com>
2024-06-01 13:23:20 -0300

    Utilidades mejora de codigo.

--- entities/utilidades.py
+++ entities/utilidades.py
@@ -15,44 +15,36 @@ def string_valido(self):
 
 
 
-def pedir_especialidad(especialidades):
-    especialidad = None
+def pedir_especialidad(especialidades):    
     while True:
         try:
             especialidad = input("    - Ingrese el nombre de la especialidad: ")
             encontrado = False
             if string_valido(especialidad):
                 encontrado = existe_verifica_objeto(especialidad, especialidades)
-                if encontrado != -1:
-                      print("\n[ (!) ERROR ] --> La especialidad ya existe, ingreselo nuevamente.\n")
-                else: break
+                if encontrado == -1: return especialidad
+                print("\n[ (!) ERROR ] --> La especialidad ya existe, ingreselo nuevamente.\n")
             else: raise ValueError
 
         except ValueError:
             print("\n[ (!) ERROR ] --> El nombre de la especialidad es incorrecto, ingréselo nuevamente.\n")
 
-    return especialidad
-
 
 
 
 def pedir_identidad(tipo):
-    identidad = None
     while True:
         try:
             identidad = input(f"    - Ingrese el {tipo}: ")
-            if string_valido(identidad): break
-            else: raise ValueError
+            if not string_valido(identidad): raise ValueError
+            return identidad
         except ValueError:
             print(f"\n[ (!) ERROR ] --> No es un {tipo} válido, ingréselo de nuevo.\n")
-
-    return identidad
 
 
 
 
 def pedir_cedula(policlinica):
-    cedula = None
     while True:
         try:
             cedula = int(input("    - Ingrese la cédula de identidad: "))
@@ -67,50 +59,44 @@ def pedir_cedula(policlinica):
                     for i in range(len(policlinica.medicos)):
                         if(policlinica.medicos[i].cedula == cedula):
                             encontrado = True
-                            break
-                        
-                if not encontrado: break
+                            break    
+                    return cedula
+                
                 else: print("\n[ (!) ERROR ] --> Esta cedula ya pertenece a un usuario registrado.\n")
             else: raise ValueError
         except ValueError:
             print("\n[ (!) ERROR ] --> No es una cédula válida, ingrese nuevamente una cédula de 8 dígitos.\n")
-    
-    return cedula
 
 
 
 
 def pedir_fecha(tipo):
-    fecha = None
     while True:
         try:
             fecha = input(f"    - Ingrese la fecha de {tipo} en formato aaaa-mm-dd: ")
             fecha = datetime.strptime(fecha, "%Y-%m-%d")
-            break
+            return fecha
         except ValueError:
             print("\n[ (!) ERROR ] --> No es una fecha válida, vuelva a ingresarla en el formato aaaa-mm-dd.\n")
-    
-    return fecha
 
 
 
 
 def pedir_celular():
-    celular = None
     while True:
         try:
             celular = input("    - Ingrese el número de celular: ")
             if not celular.isnumeric(): raise ValueError
 
-            if celular[0] == '0' and celular[1] == '9' and len(celular[2:]) == 7: break
-            else: raise ValueError
+            if celular[0] != '0' or celular[1] != '9' or len(celular[2:]) != 7: raise ValueError
+            return celular
         except ValueError:
             print("\n[ (!) ERROR ] --> No es un número de celular válido, ingrese un número con el formato 09XXXXXXX.\n")
-    
-    return celular 
+
+
+
 
 def consultar_especialidad(policlinica):
-    especialidad_obj= None
     while True:
         try:
             especialidad = input("    - Ingrese la especialidad: ")
@@ -133,57 +119,52 @@ def consultar_especialidad(policlinica):
                     else: pass
                 else:
                     especialidad_obj = policlinica.especialidades[especialidad_pos]
-                    break
+                    return especialidad_obj
 
             else: raise ValueError
         except ValueError:
             print("\n[ (!) ERROR ] -->  La especialidad debe ser un string.\n")
     
-    return especialidad_obj
+    
 
 
 def consultar_medico(policlinica, especialidad_dada):
-    medico_obj= None
     while True:
         try:
             medico = input("    - Ingrese el medico: ")
-            if string_valido(medico):
+            if not string_valido(medico): raise ValueError
 
-                medico_pos = -1
+            medico_pos = -1
 
-                for i in range(len(policlinica.medicos)):
-                    if policlinica.medicos[i].nombre.upper() + ' ' + policlinica.medicos[i].apellido.upper() == medico.upper():
-                        medico_pos = i
-                        break
-            
-                if medico_pos == -1:
-                    print("\n    El medico no está dado de alta elija una opción:\n")
-                    print("        1. Volver a ingresar el medico.")
-                    print("        2. Dar de alta esta medico.\n")
+            for i in range(len(policlinica.medicos)):
+                if policlinica.medicos[i].nombre.upper() + ' ' + policlinica.medicos[i].apellido.upper() == medico.upper():
+                    medico_pos = i
+                    break
+        
+            if medico_pos == -1:
+                print("\n    El medico no está dado de alta elija una opción:\n")
+                print("        1. Volver a ingresar el medico.")
+                print("        2. Dar de alta esta medico.\n")
 
-                    while True:
-                        try:
-                            opcion = int(input("    --> Opción: "))
-                            if  1 <= opcion <= 2: break
-                            else: raise ValueError
-                        except ValueError:
-                            print("\n[ (!) ERROR ] --> La opción seleccionada no es correcta, vuelva a intentar con otra opción.\n")
+                while True:
+                    try:
+                        opcion = int(input("    --> Opción: "))
+                        if  1 <= opcion <= 2: break
+                        else: raise ValueError
+                    except ValueError:
+                        print("\n[ (!) ERROR ] --> La opción seleccionada no es correcta, vuelva a intentar con otra opción.\n")
 
-                    if opcion == 2: policlinica.dar_alta_medico()
-                    else: pass
+                if opcion == 2: policlinica.dar_alta_medico()
+                else: pass
+            else:
+                medico_obj = policlinica.medicos[medico_pos]
+                if medico_obj.especialidad.nombre.upper() == especialidad_dada.nombre.upper():
+                    return medico_obj
                 else:
-                    medico_obj = policlinica.medicos[medico_pos]
-                    if medico_obj.especialidad.nombre.upper() == especialidad_dada.nombre.upper():
-                        break
-                    else:
-                        print("\n[ (!) ERROR ] --> La oespecialidad del medico no coincide con la especialidad consulta.\n")
-                        pass
+                    print("\n[ (!) ERROR ] --> La oespecialidad del medico no coincide con la especialidad consulta.\n")
 
-            else: raise ValueError
         except ValueError:
             print("\n[ (!) ERROR ] -->  El medico debe ser un string.\n")
-    
-    return medico_obj
     
     
 def imprimir_medicos(policlinica):
